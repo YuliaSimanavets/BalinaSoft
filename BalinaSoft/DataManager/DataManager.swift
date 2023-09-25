d14d5323d866703045689f13f897847b206d2644
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-09-25 11:20:41 +0200

    added post method [DataManager]

--- BalinaSoft/DataManager/DataManager.swift
+++ BalinaSoft/DataManager/DataManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 protocol DataManagerProtocol {
     func getData(completion: @escaping (Result<[Content], Error>) -> ())
-//    func post(uploadData: UploadData, completion: @escaping (Result<UploadData?, Error>) -> Void)
+    func upload(_ uploadData: UploadData, completion: @escaping (Result<UploadData?, Error>) -> Void)
 }
 
 final class DataManager: DataManagerProtocol {
@@ -44,5 +44,58 @@ final class DataManager: DataManagerProtocol {
             }
         }
         task.resume()
+    }
+    
+    func upload(_ uploadData: UploadData, completion: @escaping (Result<UploadData?, Error>) -> Void) {
+
+        let url = URL(string: "https://junior.balinasoft.com/api/v2/photo")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.setValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
+        request.httpBody = createBodyWithParameters(parameters: uploadData,
+                                                    filePathKey: "file",
+                                                    imageDataKey: uploadData.photo)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("error=\(String(describing: error))")
+                return
+            }
+            print("response = \(String(describing: response))")
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("response data = \(responseString!)")
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data ?? Data()) as? UploadData
+                
+                DispatchQueue.main.async {
+                    completion(.success(json))
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    private func createBodyWithParameters(parameters: UploadData, filePathKey: String?, imageDataKey: Data) -> Data {
+        let body = NSMutableData()
+        // name
+        body.appendString("\"name=\(parameters.name)\"")
+        body.appendString("\t")
+        
+        //photo
+        body.appendString("\"photo=")
+        body.append(imageDataKey)
+        body.appendString(";type=image/jpeg\"")
+        body.appendString("\t")
+        
+        //id
+        body.appendString("\"id=\(parameters.id)\"")
+        body.appendString("\t")
+        return body as Data
     }
 }
